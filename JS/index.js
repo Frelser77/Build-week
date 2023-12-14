e81f0557c71cab2c8229162f691f87f2e0c5384c
AlessandroTedde <148778362+AlessandroTedde@users.noreply.github.com>
2023-12-14 09:45:37 +0100

    minor changes

--- JS/index.js
+++ JS/index.js
@@ -34,75 +34,20 @@ playlistNames.forEach((name) => {
   randPL.appendChild(div);
 });
 
+// SCROLL BRA FUNCTION
+const navScroll = window.addEventListener("scroll", function () {
+  let nav = document.getElementById("nav");
+  if (navScroll.scrollY > 10) {
+    // Ad esempio, dopo 100px di scroll
+    nav.style.backgroundColor = "blue"; // Cambia in blu
+  } else {
+    nav.style.backgroundColor = "white"; // Torna a bianco
+  }
+});
+
 //FETCH
 // Fetch dei dati generali dell'API
 const host = "deezerdevs-deezer.p.rapidapi.com";
-
-async function main() {
-  try {
-    const response = await fetch(`https://deezerdevs-deezer.p.rapidapi.com/infos`, {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key": token,
-        "X-RapidAPI-Host": host,
-      },
-    });
-    const generalInfo = await response.json();
-    console.log(generalInfo);
-  } catch (error) {
-    console.error(error);
-  }
-
-  //'userSearchInput' sia la query inserita dall'utente
-  const userSearchInput = "Eminem"; // Sostituisci con l'input utente effettivo
-
-  const searchUrl = `https://deezerdevs-deezer.p.rapidapi.com/search?q=${encodeURIComponent(userSearchInput)}`;
-  const searchOptions = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": token,
-      "X-RapidAPI-Host": host,
-    },
-  };
-
-  try {
-    // Ricerca per l'input dell'utente e scegli un album
-    const searchResponse = await fetch(searchUrl, searchOptions);
-    const searchData = await searchResponse.json();
-
-    if (searchData && searchData.data && searchData.data.length > 0) {
-      // Visualizzo i risultati della ricerca e lascio che l'utente scelga un album
-      const albums = searchData.data.map((item) => item.album);
-      albums.forEach((album, index) => {
-        console.log(`${index + 1}: ${album.title} (ID: ${album.id})`);
-      });
-
-      // 'albumChoice' Ã¨ l'indice scelto dall'utente, input da impostare
-      const albumChoice = 1; // deve essere sostituito con l'input reale dell'utente
-      const _idAlbum = albums[albumChoice - 1].id;
-
-      // Ora recupera i dettagli dell'album scelto
-      const albumResponse = await fetch(`https://deezerdevs-deezer.p.rapidapi.com/album/${_idAlbum}`, searchOptions);
-      const albumInfo = await albumResponse.json();
-      console.log(albumInfo);
-
-      // Estraggo genre_id dall'informazione dell'album
-      if (albumInfo.genres && albumInfo.genres.data && albumInfo.genres.data.length > 0) {
-        const genreId = albumInfo.genres.data[0].id;
-        await getGenreDetails(genreId); // Chiama la funzione per ottenere i dettagli del genere
-      }
-    } else {
-      console.log("Nessun risultato trovato per la ricerca.");
-    }
-  } catch (error) {
-    console.error("Errore nella ricerca:", error);
-  }
-}
-
-main(); // Esecuzione della funzione principale
-
-//FETCH
-// Fetch dei dati generali dell'API
 
 async function getGenreDetails(genreId) {
   const urlGenres = `https://deezerdevs-deezer.p.rapidapi.com/genre/${genreId}`;
@@ -122,8 +67,26 @@ async function getGenreDetails(genreId) {
     console.error(error);
   }
 }
+
+// Funzione per creare l'HTML di una card di un album
+function createAlbumCard(album, artistName) {
+  return `
+        <div class="col g-0 m-0 card-group pointer">
+            <div class="card bg-dark rounded-3">
+                <div class="card-body mb-auto">
+                    <img src="${album.cover_medium}" class="card-img-top rounded-3" style="object-fit: cover;" alt="${album.title}" />
+                </div>
+                <div class="card-body d-flex flex-column align-item-baseline">
+                    <p class="fw-bold mb-1">${album.title}</p>
+                    <span class="text-secondary flex-grow-1">${artistName}</span>
+                </div>
+            </div>
+        </div>
+    `;
+}
+
 async function loadAlbums() {
-  const searchUrl = `https://deezerdevs-deezer.p.rapidapi.com/search?q="eminem"`; // Sostituisci "eminem" con l'artista di tua scelta
+  const artists = ["Eminem", "Adele", "Drake", "Rihanna", "Ed Sheeran", "Taylor Swift"]; // Lista di artisti
   const searchOptions = {
     method: "GET",
     headers: {
@@ -133,32 +96,24 @@ async function loadAlbums() {
   };
 
   try {
-    // Esegui la chiamata API
-    const searchResponse = await fetch(searchUrl, searchOptions);
-    const searchData = await searchResponse.json();
+    const albumsContainer = document.getElementById("albums-container");
+    albumsContainer.innerHTML = ""; // Pulisci il contenitore esistente
 
-    // Se la risposta ha dei dati, inseriscili nel DOM
-    if (searchData && searchData.data && searchData.data.length > 0) {
-      const albumsContainer = document.getElementById("albums-container");
-      albumsContainer.innerHTML = ""; // Pulisci il contenitore esistente
+    for (const artist of artists) {
+      const searchUrl = `https://deezerdevs-deezer.p.rapidapi.com/search?q="${artist}"`;
+      const searchResponse = await fetch(searchUrl, searchOptions);
+      const searchData = await searchResponse.json();
 
-      // Genera le card per ogni album e aggiungile al contenitore
-      searchData.data.forEach((album) => {
-        const albumCardHtml = `
-                    <div class="col">
-                        <div class="card bg-dark rounded-3">
-                            <img src="${album.album.cover_medium}" class="card-img-top rounded-3" width="220px" height="220px" alt="${album.album.title}" />
-                            <div class="card-body">
-                                <h5 class="card-title">${album.album.title}</h5>
-                                <p class="card-text">${album.artist.name}</p>
-                            </div>
-                        </div>
-                    </div>
-                `;
-        albumsContainer.innerHTML += albumCardHtml;
-      });
-    } else {
-      console.log("Nessun risultato trovato.");
+      if (searchData && searchData.data && searchData.data.length > 0) {
+        // Seleziona un album casuale dall'artista corrente
+        const randomAlbumIndex = Math.floor(Math.random() * searchData.data.length);
+        const album = searchData.data[randomAlbumIndex].album;
+        // Chiamata alla funzione createAlbumCard
+        const albumCardHtml = createAlbumCard(album, searchData.data[randomAlbumIndex].artist.name);
+        albumsContainer.innerHTML += albumCardHtml; // Aggiungi la card al contenitore
+      } else {
+        console.log(`Nessun risultato trovato per ${artist}.`);
+      }
     }
   } catch (error) {
     console.error("Errore nel caricamento degli album:", error);
